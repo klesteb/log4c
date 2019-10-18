2f05f8a65f8abe3b35b9fe5fa5101ce1e9cb7b2f
Kevin Esteb <kesteb@wsipc-scm-01.wise.wa-k12.net>
2019-10-18 10:43:20 -0700

    initial load

---
+++ src/log4c/appender_type_staticfile.c
@@ -0,0 +1,182 @@
+
+/*
+ * appender_type_staticfile.c
+ *
+ *
+ * See the COPYING file for the terms of usage and distribution.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#ifdef HAVE_UNISTD_H
+#include <unistd.h>
+#endif
+#include <log4c/appender.h>
+#include <log4c/appender_type_staticfile.h>
+#include <sd/malloc.h>
+#include <sd/error.h>
+#include <sd/sd_xplatform.h>
+
+/* Internal structs that defines the conf and the state info
+ * for an instance of the appender_type_staticfile type.
+ */    
+struct __staticfile_conf {
+  const char* sfc_logdir;
+  const char* sfc_files_prefix; 
+};
+
+struct __staticfile_udata {
+  struct __staticfile_conf sfu_conf;
+  char *sfu_base_filename;
+};
+
+static char *staticfile_make_base_name(const char *log_dir, const char* prefix);
+
+/***************************************************************************
+                Appender Interface functions: open, append, close
+
+****************************************************************************/
+static int staticfile_open(log4c_appender_t* this)
+{
+  int rc = 0; 
+  staticfile_udata_t *sfup = log4c_appender_get_udata(this);  
+
+  sd_debug("staticfile_appender_open[");
+
+ /* xxx Make the init here more fine grained */
+                                                       
+ if (sfup == NULL ){
+     
+   /* No info provided so set defaults */
+   sd_debug("making new staticfile conf object, with default logdir/logprefix");
+   sfup = staticfile_make_udata();
+
+   staticfile_udata_set_logdir(sfup, STATICFILE_DEFAULT_LOG_DIR);
+   staticfile_udata_set_files_prefix(sfup, STATICFILE_DEFAULT_LOG_PREFIX);
+   
+ } else {
+    sd_debug("appender has udata already: logdir='%s' logprefix='%s'",
+      staticfile_udata_get_logdir(sfup),
+      staticfile_udata_get_files_prefix(sfup));
+ }
+
+ /* build the filename  */
+ sfup->sfu_base_filename = staticfile_make_base_name(
+				       sfup->sfu_conf.sfc_logdir,
+				       sfup->sfu_conf.sfc_files_prefix);
+
+ sd_debug("]");
+      
+ return rc;
+
+}
+
+/***************************************************************************/
+
+static int staticfile_append(log4c_appender_t* this, 
+			 const log4c_logging_event_t* a_event)
+{
+  staticfile_udata_t* sfup = log4c_appender_get_udata(this); 
+  int rc = 0;
+
+  FILE *fp = NULL;
+    
+  sd_debug("staticfile_append[");
+ 
+  if ((fp = fopen(sfup->sfu_base_filename, "a+")) != NULL) {
+        
+    rc = fprintf(fp, "%s\n", a_event->evt_rendered_msg);
+    fclose(fp);
+        
+  }
+
+   sd_debug("]");
+
+  return (rc);
+
+}
+/****************************************************************************/
+static int staticfile_close(log4c_appender_t* this)
+{  
+  int rc = 0;
+
+  sd_debug("staticfile_close[");
+
+  if(!this){
+    rc = -1; 
+  }
+ 
+  sd_debug("]");
+  return(rc);
+}
+
+/*******************************************************************************/
+
+staticfile_udata_t *staticfile_make_udata(void){
+  staticfile_udata_t *sfup = NULL;
+  sfup = (staticfile_udata_t *)sd_calloc(1, sizeof(staticfile_udata_t));
+
+  return(sfup);
+}
+
+/*******************************************************************************/
+
+LOG4C_API int staticfile_udata_set_logdir(staticfile_udata_t* sfup, const char *logdir){
+
+  sfup->sfu_conf.sfc_logdir = strdup(logdir);  
+
+  return(0);
+}
+/*******************************************************************************/
+
+LOG4C_API const char * staticfile_udata_get_logdir(staticfile_udata_t* sfup){
+
+  return(sfup->sfu_conf.sfc_logdir); 
+
+}
+/*******************************************************************************/
+
+LOG4C_API int staticfile_udata_set_files_prefix( staticfile_udata_t* sfup, const char* fprefix){
+
+  sfup->sfu_conf.sfc_files_prefix = strdup(fprefix);
+
+  return(0);
+}
+/*******************************************************************************/
+
+LOG4C_API const char*  staticfile_udata_get_files_prefix( staticfile_udata_t* sfup){
+
+  return(sfup->sfu_conf.sfc_files_prefix);
+
+}
+/*******************************************************************************/
+
+static char *staticfile_make_base_name(const char *logdir, const char* prefix){
+
+  int filename_len = 0;
+  char* s = NULL;
+
+  filename_len = strlen(logdir) + 1 +
+    strlen(prefix) + 1 + 10; /* a margin */
+
+  s = (char *)malloc(filename_len);      
+  sprintf( s, "%s%s%s", logdir,
+	   FILE_SEP, prefix);
+
+  return(s);
+}
+
+/****************************************************************************/
+const log4c_appender_type_t log4c_appender_type_staticfile = {
+    "staticfile",
+    staticfile_open,
+    staticfile_append,
+    staticfile_close
+};
+
---
+++ src/log4c/appender_type_staticfile.h
@@ -0,0 +1,82 @@
+/* $Id: appender_type_staticfile.h
+ *
+ * appender_type_staticfile.h
+ * 
+ *
+ * See the COPYING file for the terms of usage and distribution.
+ */
+
+#ifndef log4c_appender_type_staticfile_h
+#define log4c_appender_type_staticfile_h
+
+/**
+ * @file appender_type_staticfile.h
+ *
+ * @brief Log4c static file appender interface.
+ *
+ * The static file appender implements a logging mechanism of
+ * opening the file, write a log message and then closeing the file.
+ *
+*/
+
+#include <log4c/defs.h>
+#include <log4c/appender.h>
+
+__LOG4C_BEGIN_DECLS
+
+#define STATICFILE_DEFAULT_LOG_DIR "."
+#define STATICFILE_DEFAULT_LOG_PREFIX "log"
+
+typedef struct __staticfile_udata staticfile_udata_t; /* opaque */
+
+/**
+ * staticfile appender type definition.
+ *
+ * This should be used as a parameter to the log4c_appender_set_type()
+ * routine to set the type of the appender.
+ *
+ **/
+LOG4C_API const log4c_appender_type_t log4c_appender_type_staticfile;
+
+/**
+ * Get a new static file appender configuration object.
+ * @return a new static file appender configuration object, otherwise NULL.
+*/
+LOG4C_API staticfile_udata_t* staticfile_make_udata(void);
+
+/**
+ * Set the logging directory in this static file appender configuration.
+ * @param sfudatap the static file appender configuration object.
+ * @param logdir the logging directory to set.
+ * @return zero if successful, non-zero otherwise.
+ */
+LOG4C_API int staticfile_udata_set_logdir(
+                staticfile_udata_t *sfudatap,
+                const char* logdir);
+/**
+ * Set the prefix string in this rolling file appender configuration.
+ * @param rfudatap the rolling file appender configuration object.
+ * @param prefix the logging files prfix to use.
+ * @return zero if successful, non-zero otherwise.
+ */                            
+LOG4C_API int staticfile_udata_set_files_prefix(
+              staticfile_udata_t *sfudatap,
+              const char * prefix);
+/*
+ * Get the logging directory in this static file appender configuration.
+ * @param sfudatap the static file appender configuration object.
+ * @return the logging directory.
+ */                              
+LOG4C_API const char* staticfile_udata_get_logdir(staticfile_udata_t* sfudatap);
+              
+/**
+ * Get the prefix string in this static file appender configuration.
+ * @param sfudatap the rolling file appender configuration object.
+ * @return the prefix.
+ */ 
+LOG4C_API const char* staticfile_udata_get_files_prefix(
+                            staticfile_udata_t* sfudatap);
+
+__LOG4C_END_DECLS
+
+#endif
